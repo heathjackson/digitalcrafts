bd753cb58fd1a52e4f2d7b24bea34f00b8349278
Heather <80366350+heathjackson@users.noreply.github.com>
2022-01-13 20:30:43 -0500

    PrimeOrNo with not needed while loop

--- week1/day4/primeOrNo.py
+++ week1/day4/primeOrNo.py
@@ -1,12 +1,15 @@
-number = 10
+number = 5
 subtract = number - 1
 
 while number > 1:
-  if subtract > 1:
-    for num in range(2, number):
-      if (number % num) == 0:
-        print("false")
-
-      else:
-        print("true")
+  if number > 1:
+    for num in range(2, subtract):
+      if (number % num) == 0:    
+        flag = True
+        break
+  else:
+    print("number = true")
   number -= 1
+
+if flag:
+  print("hello")

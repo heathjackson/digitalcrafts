0490f98b87787ca89fdf559a7f5b2111625eece1
Heather <80366350+heathjackson@users.noreply.github.com>
2022-01-18 14:00:19 -0500

    Riley's phonebook

---
+++ week2/day2/phonebook.py
@@ -0,0 +1,33 @@
+# This is my code to start a phonebook app
+
+# list []
+# tuples () - data never changes - immutable
+# set [] - only unique values
+# while/ for ... in 
+
+# dictionaries 
+
+# {key: value}
+
+# dcEmlpoyees = {
+#   "firstName": "Joseph"
+#   "lastName":  
+#   "title": "lead instructor fulltime",
+#   "location": "texas",
+#   "program": "FullTime",
+#   "activeEmlpyee": True,
+#   "programlanguages": [{
+#     "name": "python",
+#     "flkuencyGrade": 7,
+
+#   },{
+#     "name": "javascript",
+#     "fluencyGrade": 9,
+#     "update": "",
+#   }]
+# }
+
+# userInput = int(input("Press 1 to access phonebook, 2 to quit"))
+
+# while userInput != 2:
+#   userInput = int(input("Press 1 to access phonebook, 2 to quit"))
---
+++ week2/day2/phonebookExercise.py
@@ -0,0 +1,56 @@
+# phoneBook = {}
+
+# while True:
+#     userInput = int(input("""
+    
+#     Press 1 to Look up an entry
+#     press 2 to Add an entry
+#     quit program, press 3 to add a user.
+#     """))
+
+#     if userInput == 2:
+#         key = print(input("what is your last name? "))
+#         value = print(input("what is your last name? "))
+#         for key, vaue in phoneBook.items():
+#           phoneBook = 
+        
+
+
+
+
+   # elif userInput == 2:
+    #     print("Baiiii")
+    #     break
+    # elif userInput == 3:
+    #     print("lets add a user")
+
+phoneBook = {}
+while True:
+  userInput = int(input("""
+Electronic Phone Book
+===============================================
+1. Look up an entry
+2. Add a new entry
+3. Delete an entry
+4. List all entries
+5. Quit
+
+================================================
+What do you want to do (1-5)?"""))
+
+  if userInput == 1:
+    print("Look up an entry")
+  elif userInput == 2:
+    name = input ("What is the name?")
+    phoneNumber = input("What is the phone number")
+    newUser = dict ({"name" : name, "phonebook": phonebook})
+    phoneBook[name] = newUser
+    print(phoneBook)
+  elif userInput == 3:
+    nameToDelete = input("Which user would you like to delete?")
+    if nameToDelete = 
+
+
+
+  
+

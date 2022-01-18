2ee688b4120f1ba687ea83d5382a48d62e6a5e96
Heather <80366350+heathjackson@users.noreply.github.com>
2022-01-18 15:53:45 -0500

    in process of coming up with phonebank

--- week2/day2/phonebookExercise.py
+++ week2/day2/phonebookExercise.py
@@ -24,7 +24,9 @@
     # elif userInput == 3:
     #     print("lets add a user")
 
-phoneBook = {}
+
+
+phoneBook = []
 while True:
   userInput = int(input("""
 Electronic Phone Book
@@ -39,16 +41,33 @@ Electronic Phone Book
 What do you want to do (1-5)?"""))
 
   if userInput == 1:
-    print("Look up an entry")
+    nameSearch= input("Enter a name to lookup\n")
+    for entry in phoneBook:
+      if entry["name"] == nameSearch:
+        print("Found entry for", nameSearch, entry["phoneNumber"])
+
   elif userInput == 2:
     name = input ("What is the name?")
-    phoneNumber = input("What is the phone number")
-    newUser = dict ({"name" : name, "phonebook": phonebook})
-    phoneBook[name] = newUser
-    print(phoneBook)
+    phoneNumber = input ("What is the phone number?")
+    newUser = {"name": name , "phoneNumber":phoneNumber}
+    phoneBook.append(newUser)
+    print("Entry stored for", name, ".")
+
   elif userInput == 3:
-    nameToDelete = input("Which user would you like to delete?")
-    if nameToDelete = 
+    nameToDelete = input ("Which user would you like to delete? ")
+    for entry in phoneBook:
+      if entry["name"] == nameToDelete:
+        phoneBook.remove(entry)
+        print("You have removed", entry["name"], "from the phone book")
+      
+  elif userInput == 4:
+    for entry in phoneBook:
+      if entry["name"] == nameToDelete:
+        phoneBook.remove(entry)
+        print("Delted entry for", entry["name"])
+  elif userInput == 5:
+    print ("goodbye")
+    break
 
 
 

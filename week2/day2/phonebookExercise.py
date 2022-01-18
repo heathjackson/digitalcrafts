86223260bef441f4cbc131bab7456a1ed4d8f7af
Heather <80366350+heathjackson@users.noreply.github.com>
2022-01-18 18:34:09 -0500

    phonebook exercise working

--- week2/day2/phonebookExercise.py
+++ week2/day2/phonebookExercise.py
@@ -1,74 +1,65 @@
-# phoneBook = {}
-
-# while True:
-#     userInput = int(input("""
-    
-#     Press 1 to Look up an entry
-#     press 2 to Add an entry
-#     quit program, press 3 to add a user.
-#     """))
-
-#     if userInput == 2:
-#         key = print(input("what is your last name? "))
-#         value = print(input("what is your last name? "))
-#         for key, vaue in phoneBook.items():
-#           phoneBook = 
-        
-
-
-
-
-   # elif userInput == 2:
-    #     print("Baiiii")
-    #     break
-    # elif userInput == 3:
-    #     print("lets add a user")
-
-
-
 phoneBook = []
+def lookUpNumber():
+    if len(phoneBook) > 0:
+        nameSearch= input("Name: ")  
+        for entry in phoneBook:
+                if entry["name"] == nameSearch:
+                    print("Found entry for ", nameSearch.capitalize(),":", entry["phoneNumber"])
+                else:
+                    print("That name is not in the phone book!")
+    else:
+        print ("No entries in phone book yet")
+    
+def addEntry():
+    name = input("What is the name?").lower()
+    phoneNumber = input ("What is the phone number?")
+    newUser = {"name":name , "phoneNumber":phoneNumber}
+    phoneBook.append(newUser)
+    print("Entry stored for ", name.capitalize() , ".")
+
+def deleteNumber():
+    if len(phoneBook) > 0:
+        nameToDelete = input ("Which user would you like to delete? ")
+        for entry in phoneBook:
+            if entry["name"] == nameToDelete:
+                phoneBook.remove(entry)
+                print("Deleted entry for", entry["name"])
+            else:
+                print("That name is not in the phone book!")
+                return
+    else:
+        print ("No entries in phone book")
+
+def listAllEntries ():
+    if len(phoneBook) > 0:
+        for entry in phoneBook:
+          entry = entry['name']
+          print ("Found entry for ", entry["name"],":", entry["phoneNumber"])
+    else:
+        print ("No entries in phone book yet")
+        
 while True:
-  userInput = int(input("""
+    userInput = int(input("""
 Electronic Phone Book
-===============================================
+=====================
 1. Look up an entry
 2. Add a new entry
 3. Delete an entry
 4. List all entries
-5. Quit
-
-================================================
+5. Quit 
+=====================
 What do you want to do (1-5)?"""))
-
-  if userInput == 1:
-    nameSearch= input("Enter a name to lookup\n")
-    for entry in phoneBook:
-      if entry["name"] == nameSearch:
-        print("Found entry for", nameSearch, entry["phoneNumber"])
-
-  elif userInput == 2:
-    name = input ("What is the name?")
-    phoneNumber = input ("What is the phone number?")
-    newUser = {"name": name , "phoneNumber":phoneNumber}
-    phoneBook.append(newUser)
-    print("Entry stored for", name, ".")
-
-  elif userInput == 3:
-    nameToDelete = input ("Which user would you like to delete? ")
-    for entry in phoneBook:
-      if entry["name"] == nameToDelete:
-        phoneBook.remove(entry)
-        print("You have removed", entry["name"], "from the phone book")
-      
-  elif userInput == 4:
-    for entry in phoneBook:
-      if entry["name"] == nameToDelete:
-        phoneBook.remove(entry)
-        print("Delted entry for", entry["name"])
-  elif userInput == 5:
-    print ("goodbye")
-    break
-
+    if userInput == 1:
+        lookUpNumber()
+    elif userInput == 2:
+        addEntry()
+    elif userInput == 3:
+        deleteNumber()
+    elif userInput == 4:
+        listAllEntries()
+    elif userInput == 5:
+        print ("Bye.")
+        break
 
 
   
